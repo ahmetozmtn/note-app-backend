becd67502303f2bccc52830e453a150031bfbd33
ahmetozmtn <ahmethack13@gmail.com>
2025-09-04 15:52:01 +0300

    feat: Enhance Express server with middleware for CORS, body parsing, and security; add a basic route

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
 import connectDB from './config/db.js';
 
 const app = express();
 
-app.use(express.json());
-
+// Middlewares
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(helmet());
 // Connect to MongoDB
 connectDB();
+
+app.get('/', (req, res) => {
+    res.status(200).send({ message: 'Hello World' });
+});
 
 export default app;
