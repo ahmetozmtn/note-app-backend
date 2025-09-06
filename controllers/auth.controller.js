dc62f444620a969b9b19c8bed837303805cd87f8
ahmetozmtn <ahmethack13@gmail.com>
2025-09-06 18:30:23 +0300

    feat: Implement register endpoint (`/api/auth/register`)

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,35 @@
+import bcrypt from 'bcrypt';
+
 import User from '../models/user.model.js';
 
 export const register = async (req, res) => {
-    const { name, email, password } = req.body;
-    const user = await User.create({ name, email, password });
-    res.status(201).json({ message: 'User registered', data: user });
+    try {
+        const { name, email, password } = req.body;
+        const userMailCheck = await User.findOne({ email });
+        if (userMailCheck) {
+            return res.status(409).json({ message: 'Email already exists' });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+        delete user.password;
+        res.status(201).json({
+            message: 'User registered',
+            data: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                createdAt: user.createdAt,
+                updatedAt: user.updatedAt,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error',
+            error: error.message,
+        });
+    }
 };
