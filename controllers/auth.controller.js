700bfbfbb143d2a2e7eccd1dea8f86b946d41840
ahmetozmtn <ahmethack13@gmail.com>
2025-09-06 18:52:01 +0300

    feat: Implement login endpoint (`/api/auth/login`) with validation

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -33,3 +33,35 @@ export const register = async (req, res) => {
         });
     }
 };
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        console.log(req.body);
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res
+                .status(401)
+                .json({ message: 'Invalid email or password' });
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res
+                .status(401)
+                .json({ message: 'Invalid email or password' });
+        }
+        return res.status(200).json({
+            message: 'Login successful',
+            data: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            },
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error',
+            error: error.message,
+        });
+    }
+};
