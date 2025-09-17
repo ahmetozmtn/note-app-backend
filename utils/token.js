e837b20eba0a9f8bdb48539a930f75bc9e4c88e6
ahmetozmtn <ahmethack13@gmail.com>
2025-09-17 10:12:31 +0300

    refactor: Simplify user model and enhance token generation
    - Removed the 'select: false' option from the password field in the user model to allow password retrieval when necessary.
    - Updated the token generation function to accept only the user ID, improving clarity and security.
    - Enhanced the token verification function to throw an error for invalid tokens, ensuring better error handling.

--- utils/token.js
+++ utils/token.js
@@ -1,10 +1,14 @@
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET_KEY, JWT_EXPIRES_IN } from '../config/env.js';
 
-export const generateToken = payload => {
-    return jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: JWT_EXPIRES_IN });
+export const generateToken = ({ id }) => {
+    return jwt.sign({ id }, JWT_SECRET_KEY, { expiresIn: JWT_EXPIRES_IN });
 };
 
 export const verifyToken = token => {
-    return jwt.verify(token, JWT_SECRET_KEY);
+    try {
+        return jwt.verify(token, JWT_SECRET_KEY);
+    } catch (error) {
+        throw new Error('Invalid token');
+    }
 };
