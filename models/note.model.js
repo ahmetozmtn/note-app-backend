5700c90cdb2c0b28532be9bae728f80f0f58964f
ahmetozmtn <ahmethack13@gmail.com>
2025-09-21 21:29:16 +0300

    refactor: Update note and user models to use timestamps
    - Modified NoteSchema and UserSchema to include timestamps for createdAt and updatedAt fields, enhancing data tracking and consistency across models.

--- models/note.model.js
+++ models/note.model.js
@@ -1,18 +1,21 @@
 import mongoose from 'mongoose';
 
-const NoteSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
+const NoteSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        content: { type: String, required: true },
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        tags: { type: [String], default: [] },
+        color: { type: String, default: '#000000' },
+        createdAt: { type: Date, default: Date.now },
+        updatedAt: { type: Date, default: Date.now },
     },
-    tags: { type: [String], default: [] },
-    color: { type: String, default: '#000000' },
-});
+    { timestamps: true }
+);
 
 const Note = mongoose.model('Note', NoteSchema);
 export default Note;
