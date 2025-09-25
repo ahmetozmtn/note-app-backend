e902d0b7a44638605820131c7deb91975c937158
ahmetozmtn <ahmethack13@gmail.com>
2025-09-25 22:20:58 +0300

    feat: Implement graceful shutdown for server
    - Added SIGTERM and SIGINT signal handlers to gracefully shut down the server, ensuring ongoing requests are completed before exiting.
    - Improved server management and reliability during deployment and maintenance.

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 import app from './app.js';
 import { PORT } from './config/env.js';
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+process.on('SIGTERM', () => {
+    console.log('SIGTERM signal received');
+    server.close(() => {
+        console.log('Server is shutting down');
+        process.exit(0);
+    });
+});
+
+process.on('SIGINT', () => {
+    console.log('SIGINT signal received');
+    server.close(() => {
+        console.log('Server is shutting down');
+        process.exit(0);
+    });
 });
