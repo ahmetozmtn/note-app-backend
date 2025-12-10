530e890a408b8c3624913f5dd271854f32956a20
ahmetozmtn <ahmethack13@gmail.com>
2025-12-10 22:32:05 +0300

    feat: Enhance token generation and verification error handling

--- utils/token.js
+++ utils/token.js
@@ -5,16 +5,26 @@ import {
     EMAIL_TOKEN_SECRET_KEY,
 } from '../config/env.js';
 
-export const generateToken = ({ id }) =>
-    jwt.sign({ id }, JWT_SECRET_KEY, { expiresIn: JWT_EXPIRES_IN });
+export const generateToken = ({ id }) => {
+    try {
+        return jwt.sign({ id }, JWT_SECRET_KEY, { expiresIn: JWT_EXPIRES_IN });
+    } catch (error) {
+        throw new Error(error);
+    }
+};
 
-export const generateEmailToken = ({ id }) =>
-    jwt.sign({ id }, EMAIL_TOKEN_SECRET_KEY, { expiresIn: '15m' });
+export const generateEmailToken = ({ id }) => {
+    try {
+        return jwt.sign({ id }, EMAIL_TOKEN_SECRET_KEY, { expiresIn: '15m' });
+    } catch (error) {
+        throw new Error(error);
+    }
+};
 
 export const verifyToken = token => {
     try {
         return jwt.verify(token, JWT_SECRET_KEY);
     } catch (error) {
-        throw new Error('Invalid token');
+        throw new Error(error);
     }
 };
