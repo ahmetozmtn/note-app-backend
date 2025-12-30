c763eabc9096bdcf63620086cade75008b85a276
ahmetozmtn <ahmethack13@gmail.com>
2025-12-30 18:58:16 +0300

    refactor: simplify mongoose connection by removing unnecessary options

--- config/db.js
+++ config/db.js
@@ -7,10 +7,7 @@ if (!MONGO_URI) {
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(MONGO_URI);
         console.log('MongoDB connected...');
     } catch (error) {
         console.log(error);
